266927ca99ad3d5a3cd48b7b4b2a6906b3817a22
Nagin <naginbanodha@gmail.com>
2025-12-16 15:15:24 +0530

    udpated code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,8 +14,10 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: '*',
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
   });
 
   app.useGlobalPipes(
